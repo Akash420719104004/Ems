60796d95d8336d40f0a8fe35aaf1eabc0a4c3d18
Elcot <sakash8151@gmail.com>
2025-04-20 22:25:57 +0530

    First Commit message

--- demo1/demo1/src/main/java/com/mySql2/demo1/repository/EmployeeRepository.java
+++ demo1/demo1/src/main/java/com/mySql2/demo1/repository/EmployeeRepository.java
@@ -13,5 +13,6 @@ public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     @Query("SELECT e FROM Employee e WHERE e.name = :name AND e.mobileNo = :mobileNo")
     Optional<Employee> findByEmailAndMobile(String mobileNo, String name);
     @Query("SELECT e FROM Employee e WHERE e.name LIKE %:name%")
+
     List<Employee> findBysearch(String name);
 }
